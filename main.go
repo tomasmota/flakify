f22da5ffa992f2f4652c23283f9aa2cfad5065c2
tomasmota <tomasrebelomota@gmail.com>
2024-03-17 07:39:48 +0100

    fmt

--- main.go
+++ main.go
@@ -7,33 +7,33 @@ import (
 	"slices"
 )
 
-func main()  {
-    err := exec.Command("nix", "--version").Run()
-    if err != nil {
-	log.Fatal("Nix is not installed in the system")
-    }
-    
-    if ! flakesEnabled() {
-	log.Fatal("Flakes are not enabled")
-    }
+func main() {
+	err := exec.Command("nix", "--version").Run()
+	if err != nil {
+		log.Fatal("Nix is not installed in the system")
+	}
+
+	if !flakesEnabled() {
+		log.Fatal("Flakes are not enabled")
+	}
 }
 
 type NixConfig struct {
-    ExperimentalFeatures ExperimentalFeatures `json:"experimental-features"`
+	ExperimentalFeatures ExperimentalFeatures `json:"experimental-features"`
 }
 
 type ExperimentalFeatures struct {
-    Values []string `json:"value"`
+	Values []string `json:"value"`
 }
 
 func flakesEnabled() bool {
-    out, err := exec.Command("nix", "show-config", "--json").Output()
-    if err != nil {
-	log.Fatal("ERROR")
-    }
+	out, err := exec.Command("nix", "show-config", "--json").Output()
+	if err != nil {
+		log.Fatal("ERROR")
+	}
 
-    config := &NixConfig{}
-    json.Unmarshal(out, config)
+	config := &NixConfig{}
+	json.Unmarshal(out, config)
 
-    return slices.Contains(config.ExperimentalFeatures.Values, "flakes")
+	return slices.Contains(config.ExperimentalFeatures.Values, "flakes")
 }
